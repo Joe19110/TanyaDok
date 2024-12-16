f3a323267c86bd7d251b4abd501dc0a250391cc1
LyonelJS <lyonel.saputra@binus.ac.id>
2024-12-16 23:05:50 +0700

    Update register.js

--- resources/js/register.js
+++ resources/js/register.js
@@ -1,18 +1,4 @@
-// Import the functions you need from the SDKs you need
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Database config
 const firebaseConfig = {
     apiKey: "AIzaSyD9lP6HWENT1eoaPubNdc2BJKAZP6uEtgs",
     authDomain: "login-93b7c.firebaseapp.com",
@@ -33,18 +19,17 @@ const db = firebase.database();
 const getin = document.getElementById('getin');
 getin.addEventListener("click", function(event){
     event.preventDefault()
-    //inputs
+    //get the user inputs
     const email = document.getElementById('email-signin').value;
     const password = document.getElementById('password-signin').value;
-    auth.signInWithEmailAndPassword(email, password)
+    auth.signInWithEmailAndPassword(email, password) // Checks the email and password according to the one stored in db
     .then(() => {
       // Signed in 
       alert("Logging in...")
-      window.location.href="dashboard.html";
-      // ...
+      window.location.href="dashboard.html"; // Go to the dashbord html after loggin in
     })
     .catch(() => {
-      const errorMessage = 'Wrong Password or Username';
+      const errorMessage = 'Wrong Password or Username'; // Alert that the user input wrong password or username
       alert(errorMessage)
     });
 })
@@ -63,7 +48,7 @@ enlist.addEventListener("click",function(event){
         // Signed up 
         alert("Creating Account...")
         
-        const userEmail = userCredential.user.email.replace(/[^a-zA-Z0-9]/g, ','); // Retrieve the email
+        const userEmail = userCredential.user.email.replace(/[^a-zA-Z0-9]/g, ','); // Store the email in database
         db.ref(`users/${userEmail}/username`).set(username)    
     })
     .then(()=>{
@@ -73,26 +58,29 @@ enlist.addEventListener("click",function(event){
     .catch((error) => {
         const errorMessage = error.message;
         alert(errorMessage)
-        // ..
     });
 })
 
+// Sign in with google
+// Initialize the google auth
 const googleProvider = new firebase.auth.GoogleAuthProvider();
-
-    const googleSignInBtn = document.getElementById('signin-google');
-    googleSignInBtn.addEventListener('click', () => {
-      firebase.auth().signInWithPopup(googleProvider)
-        .then((result) => {
-          const user = result.user;
-          const userEmail = user.email;
-          console.log("User's email:", userEmail);
-        })
-        .then(() => {
-          // Signed in 
-          alert("Logging in...")
-          window.location.href="dashboard.html";
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+// Shows the google sign in when clicking the button
+const googleSignInBtn = document.getElementById('signin-google');
+googleSignInBtn.addEventListener('click', () => {
+  firebase.auth().signInWithPopup(googleProvider)
+    .then((result) => {
+      const user = result.user; // Gets the user data from the google account
+      const userEmail = user.email;
+      console.log("User's email:", userEmail);
+    })
+    .then(() => {
+      // Signed in alert
+      alert("Logging in...")
+      window.location.href="dashboard.html";
+    })
+    .catch((error) => {
+      console.error(error);
     });
+});
+
+ 
